323bf29860dd987589695ed32ff16e9c0fd9f70e
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-05 16:01:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 require_once __DIR__ . '/lineBot.php';
 
 $bot = new Linebot();
-$text = $bot->getMessageText();
-$text = $bot->getProfile();
-
+$text1 = $bot->getMessageText();
+$text2 = $bot->getProfile();
+$text = $text1 + $text2;
 $bot->reply($text);
 
 
