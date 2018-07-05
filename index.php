9f6379dc86b89b076a6d17658775a512b85b3b00
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-05 22:10:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,8 @@ require_once __DIR__ . '/lineBot.php';
 $bot = new Linebot();
 $msg = $bot->getMessageText();
 $userid = $bot->getUserId();
-$name = $bot->getProfile($userid);
-
-//$bot->reply($name." --> ".$msg);
+$profile = $bot->getProfile($userid);
+$name = $profile['displayName'];
 $bot->reply($name." --> ".$msg);
 
 //echo $name;
