f53bdd7516c5e130ab35f097c582e943cff6fdb5
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-07 19:54:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,16 +9,16 @@ $profile_obj = json_decode($profile);
 
 //$bot->reply($profile_obj->{'displayName'}." --> ".$msg);
 
-$queue = $bot->getQ("B0002", "S0012");
-$queue_obj = json_decode($queue);
-$qnumber = $queue_obj->{'queue'}->{'queueNumber'};
-$esttime = $queue_obj->{'queue'}->{'estimateTime'};
-$qbefore = $queue_obj->{'queue'}->{'queueBefore'};
-$bot->replyFlex($userid, $profile_obj->{'displayName'}, $qnumber, $esttime, $qbefore);
+//$queue = $bot->getQ("B0002", "S0012");
+//$queue_obj = json_decode($queue);
+//$qnumber = $queue_obj->{'queue'}->{'queueNumber'};
+//$esttime = $queue_obj->{'queue'}->{'estimateTime'};
+//$qbefore = $queue_obj->{'queue'}->{'queueBefore'};
+//$bot->replyFlex($userid, $profile_obj->{'displayName'}, $qnumber, $esttime, $qbefore);
 
-//$service = $bot->getServiceQ("B0002");
-//$service_obj = json_decode($service);
-//$bot->reply($service_obj);
+$service = $bot->getServiceQ("B0002");
+$service_obj = json_decode($service);
+$bot->reply($service_obj);
 
 
 
