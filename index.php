199c5f4ebec725809d36d68d7c411f552af94cf5
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-07 14:19:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ $obj1 = json_decode($profile);
 
 //$bot->reply($obj1->{'displayName'}." --> ".$msg);
 
-//$queue = $bot->getQ();
+//$queue = $bot->getQ("B0002", "S0012");
 //$obj2 = json_decode($queue);
 //$qnumber = $obj2->{'queue'}->{'queueNumber'};
 //$esttime = $obj2->{'queue'}->{'estimateTime'};
 //$qbefore = $obj2->{'queue'}->{'queueBefore'};
 //$bot->replyFlex($userid, $obj1->{'displayName'}, $qnumber, $esttime, $qbefore);
 
-$service = $bot->getServiceQ();
+$service = $bot->getServiceQ("B0002");
 $bot->reply($service);
 
 
