1f763688760067118c5e68d65b8137a27e32b7dc
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-07 20:23:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,14 @@ $profile_obj = json_decode($profile);
 
 $service = $bot->getServiceQ("B0002");
 $service_obj = json_decode($service);
-$bot->reply($service_obj->{'services'}[0]->{'serviceDesc'});
+
+$desc = "";
+$i = 1;
+foreach($service_obj->{'services'} as $item) {
+    $desc = $desc.$i.") ".$item['groupID']." | ".$item['serviceDesc']."\n";    
+    $i++;
+}
+$bot->reply($desc);
 //$bot->reply("tEST");
 
 
