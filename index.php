4b79f34fefaf4c614af218f8917b052a12cb8213
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-08 14:12:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,33 +2,43 @@
 require_once __DIR__ . '/lineBot.php';
 
 $bot = new Linebot();
+
+// Get User Info from Line
 $msg = $bot->getMessageText();
 $userid = $bot->getUserId();
 $profile = $bot->getProfile($userid);
 $profile_obj = json_decode($profile);
-
 //$bot->reply($profile_obj->{'displayName'}." --> ".$msg);
 
-//$queue = $bot->getQ("B0002", "S0012");
-//$queue_obj = json_decode($queue);
-//$qnumber = $queue_obj->{'queue'}->{'queueNumber'};
-//$esttime = $queue_obj->{'queue'}->{'estimateTime'};
-//$qbefore = $queue_obj->{'queue'}->{'queueBefore'};
-//$bot->replyFlex($userid, $profile_obj->{'displayName'}, $qnumber, $esttime, $qbefore);
-
-$service = $bot->getServiceQ("B0002");
+// Get Service from BranchID
+$branchID = "B0002";
+$service = $bot->getServiceQ($branchID);
 $service_obj = json_decode($service);
-
 
 $cnt = count($service_obj->{'services'});
 $desc = "";
+$chk = false;
 for ($i=0; $i<$cnt; $i++) {
     $j = $i+1;
-    //$desc = $desc."กด Q".$j." เพื่อจองคิว ".$service_obj->{'services'}[$i]->{'groupID'}." | ".$service_obj->{'services'}[$i]->{'serviceDesc'}."\n";
-    $desc = $desc."กด Q".$j." ".$service_obj->{'services'}[$i]->{'serviceDesc'}."\n";
+    //$desc = $desc."พิมพ์ ".$j." เพื่อจองคิว ".$service_obj->{'services'}[$i]->{'groupID'}." | ".$service_obj->{'services'}[$i]->{'serviceDesc'}."\n";
+    $desc = $desc."พิมพ์ ".$j." ".$service_obj->{'services'}[$i]->{'serviceDesc'}."\n";
+    
+    if ($msg == $j) {         
+        $chk = true;
+        $groupid = $service_obj->{'services'}[$i]->{'groupID'};             
+    }
 }
-$bot->replyFlexMenu($userid, $profile_obj->{'displayName'}, $desc);
 
+if ($chk == true) {
+    $queue = $bot->getQ($branchID, $groupid);
+    $queue_obj = json_decode($queue);
+    $qnumber = $queue_obj->{'queue'}->{'queueNumber'};
+    $esttime = $queue_obj->{'queue'}->{'estimateTime'};
+    $qbefore = $queue_obj->{'queue'}->{'queueBefore'};
+    $bot->replyFlex($userid, $profile_obj->{'displayName'}, $qnumber, $esttime, $qbefore);   
+} else {
+    $bot->replyFlexMenu($userid, $profile_obj->{'displayName'}, $desc);
+}
 
 //$bot->reply($service_obj->{'services'}[0]->{'serviceDesc'});
 //$bot->reply("tEST");
