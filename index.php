a99fcc1a096b23a401259f9f91a7dbf183a51ec6
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-10 09:43:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -58,6 +58,19 @@ if ($chk == 1) {
 } elseif ((chk == 0) && (($msg == "X") || ($msg == "x"))) {
     $bot->setCancelQ($userid);
     $bot->reply("คิวที่ท่านจองได้ถูกยกเลิกเรียบร้อย.");
+} elseif ((chk == 0) && ($msg == "GetAmt")) {
+    $servAmt = $bot->getAmtServiceQ($branch);
+    $servAmt_obj = json_decode($service);
+    $cnt = count($servAmt_obj->{'services'});
+    
+    $desc = "";
+    $chk = 0;
+    for ($i=0; $i<$cnt; $i++) {
+        $j = $i+1;        
+        $desc = $desc.$servAmt_obj->{'services'}[$i]->{'serviceDesc'}." มีจำนวน ".$servAmt_obj->{'services'}[$i]->{'queueNumber'}." คิว\n";
+    }
+    $bot->replyFlexMenu($userid, $profile_obj->{'displayName'}, $desc);
+    
 } else {
     $bot->replyFlexMenu($userid, $profile_obj->{'displayName'}, $desc);
 }
