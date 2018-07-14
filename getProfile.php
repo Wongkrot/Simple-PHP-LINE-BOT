703233553e0032e177df65502522bef52cb4b426
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-15 01:15:05 +0700

    Update getProfile.php

--- getProfile.php
+++ getProfile.php
@@ -8,7 +8,7 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 
 
 $headers = array();
-$headers[] = "Authorization: Bearer {5ZXZrSS8ZkEDGBilHGY2vKyWARc0Yx6O9zOZ/iN24MH39flQZLWt7gvvOY10/LMD3rppnVJBza1RyQIMXJ2vsoPh8i+L2nyIG8y0tlqR/asJiq0gfm1W5wh93re+XESxhwpUoa5q3iZuokzvYqNIcgdB04t89/1O/w1cDnyilFU=}";
+$headers[] = "Authorization: Bearer 5ZXZrSS8ZkEDGBilHGY2vKyWARc0Yx6O9zOZ/iN24MH39flQZLWt7gvvOY10/LMD3rppnVJBza1RyQIMXJ2vsoPh8i+L2nyIG8y0tlqR/asJiq0gfm1W5wh93re+XESxhwpUoa5q3iZuokzvYqNIcgdB04t89/1O/w1cDnyilFU=";
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 $result = curl_exec($ch);
