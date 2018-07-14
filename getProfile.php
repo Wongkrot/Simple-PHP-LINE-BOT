9e7119a00859e5e197ca0b4688e4833db5610b9b
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-15 01:52:20 +0700

    Update getProfile.php

--- getProfile.php
+++ getProfile.php
@@ -6,7 +6,7 @@ $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, "https://api.line.me/oauth2/v2.1/token");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=authorization_code&code=".$code.";&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1591950917&client_secret=33a71e2a76f6b89b4b31e61f02d93f97");
+curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=authorization_code&code=".$code.";&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1594277893&client_secret=d2a63d13dacf5041464d249127ebf50d");
 curl_setopt($ch, CURLOPT_POST, 1);
 
 $headers = array();
