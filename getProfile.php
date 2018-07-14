a803bb6fff8fbdfd6a1905a4531ef97b074992e5
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-14 23:45:16 +0700

    Update getProfile.php

--- getProfile.php
+++ getProfile.php
@@ -2,12 +2,11 @@
 require_once __DIR__ . '/lineBot.php';
 $bot = new Linebot();
 // Get User Info from Line
-$userid = $bot->getUserId();
-$profile = $bot->getProfile($userid);
+$profile = $bot->getProfileLine();
 $profile_obj = json_decode($profile);
 
 //header('Location: http://www.d-sci.co.th/LineQ.html?profile='.$profile_obj->{'displayName'}); 
-echo $userid; 
-echo $profile_obj->{'displayName'}; 
+
+echo $profile_obj->{'userId'}." : ".$profile_obj->{'displayName'}; 
 
     
