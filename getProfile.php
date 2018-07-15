87f4dda77884497531d7f84634a756170b0dbe34
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-15 14:47:51 +0700

    Update getProfile.php

--- getProfile.php
+++ getProfile.php
@@ -25,13 +25,13 @@ curl_close ($ch); */
 
         $ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL, "https://api.line.me/v2/profile");
+	curl_setopt($ch, CURLOPT_URL, "https://api.line.me/v1/profile");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 
 
 	$headers = array();
-	$headers[] = "Authorization: Bearer {d2a63d13dacf5041464d249127ebf50d}";
+	$headers[] = "Authorization: Bearer {5ZXZrSS8ZkEDGBilHGY2vKyWARc0Yx6O9zOZ/iN24MH39flQZLWt7gvvOY10/LMD3rppnVJBza1RyQIMXJ2vsoPh8i+L2nyIG8y0tlqR/asJiq0gfm1W5wh93re+XESxhwpUoa5q3iZuokzvYqNIcgdB04t89/1O/w1cDnyilFU=}";
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 	$result = curl_exec($ch);
