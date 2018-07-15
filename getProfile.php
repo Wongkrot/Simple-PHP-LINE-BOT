17d7b110239ceb2d60069322e95fabf5f7054332
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-15 13:38:23 +0700

    Update getProfile.php

--- getProfile.php
+++ getProfile.php
@@ -6,7 +6,7 @@ $api = "https://api.line.me/oauth2/v2.1/token";
 $authen_login ="grant_type=authorization_code&code=$code;&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1594277893&client_secret=d2a63d13dacf5041464d249127ebf50d";
 $authen_api ="grant_type=authorization_code&code=$code;&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1591950917&client_secret=33a71e2a76f6b89b4b31e61f02d93f97";
 
-/*$ch = curl_init();
+$ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, $api);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -21,20 +21,9 @@ $result = curl_exec($ch);
 if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
-curl_close ($ch);*/
+curl_close ($ch);
 
-try {
-    $oauth = new OAuth(OAUTH_CONSUMER_KEY,OAUTH_CONSUMER_SECRET);
-    $oauth->setToken($request_token,$request_token_secret);
-    $access_token_info = $oauth->getAccessToken("https://api.line.me/oauth2/v2.1/token");
-    if(!empty($access_token_info)) {
-        print_r($access_token_info);
-    } else {
-        print "Failed fetching access token, response was: " . $oauth->getLastResponse();
-    }
-} catch(OAuthException $E) {
-    echo "Response: ". $E->lastResponse . "\n";
-}
+
 
 //header('Location: http://www.d-sci.co.th/LineQ.html?profile='.$profile_obj->{'displayName'}); 
 
