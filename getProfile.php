5fda5846a0c2e40909d3c5fcebdbe28aff9e1164
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-15 14:16:32 +0700

    Update getProfile.php

--- getProfile.php
+++ getProfile.php
@@ -6,12 +6,29 @@ $api = "https://api.line.me/oauth2/v2.1/token";
 $authen_login ="grant_type=authorization_code&code=$code;&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1594277893&client_secret=d2a63d13dacf5041464d249127ebf50d";
 $authen_api ="grant_type=authorization_code&code=$code;&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1591950917&client_secret=33a71e2a76f6b89b4b31e61f02d93f97";
 
-$ch = curl_init();
+/* $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, "https://api.line.me/oauth2/v2.1/token");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $authen_login);
 curl_setopt($ch, CURLOPT_POST, 1);
+
+$headers = array();
+$headers[] = "Content-Type: application/x-www-form-urlencoded";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch); */
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://api.line.me/oauth2/v2.1/verify?access_token=5ZXZrSS8ZkEDGBilHGY2vKyWARc0Yx6O9zOZ/iN24MH39flQZLWt7gvvOY10/LMD3rppnVJBza1RyQIMXJ2vsoPh8i+L2nyIG8y0tlqR/asJiq0gfm1W5wh93re+XESxhwpUoa5q3iZuokzvYqNIcgdB04t89/1O/w1cDnyilFU=");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "client_secret=d2a63d13dacf5041464d249127ebf50d");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+
 
 $headers = array();
 $headers[] = "Content-Type: application/x-www-form-urlencoded";
