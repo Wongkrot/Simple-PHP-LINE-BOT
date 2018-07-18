c823297faaf7068a1c1e6e96b2c289a4b4ab6eb9
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-18 13:59:48 +0700

    Update getProfile.php

--- getProfile.php
+++ getProfile.php
@@ -3,8 +3,10 @@
 $code = $_GET['code'];
 $api = "https://api.line.me/oauth2/v2.1/token";
 
-$authen_login ="grant_type=authorization_code&code=$code;&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1594277893&client_secret=d2a63d13dacf5041464d249127ebf50d";
-$authen_api ="grant_type=authorization_code&code=$code;&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1591950917&client_secret=33a71e2a76f6b89b4b31e61f02d93f97";
+//$authen_login ="grant_type=authorization_code&code=$code;&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1594277893&client_secret=d2a63d13dacf5041464d249127ebf50d";
+//$authen_api ="grant_type=authorization_code&code=$code;&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1591950917&client_secret=33a71e2a76f6b89b4b31e61f02d93f97";
+
+$userid = $bot->getUserId();
 
 /* $ch = curl_init();
 
@@ -41,30 +43,11 @@ curl_close ($ch); */
 	}
 	curl_close ($ch);*/
 
-	$ch = curl_init();
-
-	curl_setopt($ch, CURLOPT_URL, "https://api.line.me/v2/profile");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-
-
-	$headers = array();
-	$headers[] = "Authorization: Bearer {5ZXZrSS8ZkEDGBilHGY2vKyWARc0Yx6O9zOZ/iN24MH39flQZLWt7gvvOY10/LMD3rppnVJBza1RyQIMXJ2vsoPh8i+L2nyIG8y0tlqR/asJiq0gfm1W5wh93re+XESxhwpUoa5q3iZuokzvYqNIcgdB04t89/1O/w1cDnyilFU=}";
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-	$result = curl_exec($ch);
-	if (curl_errno($ch)) {
-	    echo 'Error:' . curl_error($ch);
-	}
-	curl_close ($ch);
-
-
-
 //header('Location: http://www.d-sci.co.th/LineQ.html?profile='.$profile_obj->{'displayName'}); 
 
 echo "<br> <br> <br>";
 
-echo $result." <br>";
+echo $userid." <br>";
 
 echo "$code : Test <br> ";
 
