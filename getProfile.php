b79392a172cb248180b5bdbc9b39c2dbeca6e01c
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-18 14:42:32 +0700

    Update getProfile.php

--- getProfile.php
+++ getProfile.php
@@ -8,39 +8,32 @@ $authen_login ="grant_type=authorization_code&code=$code&redirect_uri=https://ma
 
 
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL, "https://api.line.me/oauth2/v2.1/token");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $authen_login);
 curl_setopt($ch, CURLOPT_POST, 1);
-
 $headers = array();
 $headers[] = "Content-Type: application/x-www-form-urlencoded";
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
 $result = curl_exec($ch);
 if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
 curl_close ($ch); 
 
-        /*$ch = curl_init();
-
-	//curl_setopt($ch, CURLOPT_URL, "https://api.line.me/v2/bot/profile/U7b0404c29adc3a97d6db31dff5987f8a");
-	curl_setopt($ch, CURLOPT_URL, "https://api.line.me/v2/profile");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-
-
-	$headers = array();
-	$headers[] = "Authorization: Bearer {5ZXZrSS8ZkEDGBilHGY2vKyWARc0Yx6O9zOZ/iN24MH39flQZLWt7gvvOY10/LMD3rppnVJBza1RyQIMXJ2vsoPh8i+L2nyIG8y0tlqR/asJiq0gfm1W5wh93re+XESxhwpUoa5q3iZuokzvYqNIcgdB04t89/1O/w1cDnyilFU=}";
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-	$result = curl_exec($ch);
-	if (curl_errno($ch)) {
-	    echo 'Error:' . curl_error($ch);
-	}
-	curl_close ($ch);*/
+       
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://api.line.me/v2/profile");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+$headers = array();
+$headers[] = "Authorization: Bearer {$result->{'access_token'}}";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
 
 //header('Location: http://www.d-sci.co.th/LineQ.html?profile='.$profile_obj->{'displayName'}); 
 
