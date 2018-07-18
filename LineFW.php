b0e5021e828d5d3efe88922f0b01e8d35f54888f
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-18 16:22:08 +0700

    Update LineFW.php

--- LineFW.php
+++ LineFW.php
@@ -1,8 +1,8 @@
 <?php
 $code = $_GET['code'];
-//$api = "https://api.line.me/oauth2/v2.1/token";
+
 $authen_login ="grant_type=authorization_code&code=$code&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1594277893&client_secret=d2a63d13dacf5041464d249127ebf50d";
-//$authen_api ="grant_type=authorization_code&code=$code&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1591950917&client_secret=33a71e2a76f6b89b4b31e61f02d93f97";
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://api.line.me/oauth2/v2.1/token");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -18,8 +18,7 @@ if (curl_errno($ch)) {
 curl_close ($ch); 
 $result_obj = json_decode($result);
 $acctoken = $result_obj->{'access_token'};
-//echo "Token :  $acctoken <br>";
-//echo "Hello Test";
+
 $ch1 = curl_init();
 curl_setopt($ch1, CURLOPT_URL, "https://api.line.me/v2/profile");
 curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
@@ -35,4 +34,6 @@ curl_close ($ch1);
 $result_obj = json_decode($result);
 $userid = $result_obj->{'userId'};
 $displayname = $result_obj->{'displayName'};
-header("Location: http://www.d-sci.co.th/LineQ.html?name=$displayname"); 
+// header("Location: http://www.d-sci.co.th/LineQ.html?name=$displayname"); 
+
+echo "$userid : $displayname";
