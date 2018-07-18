09271671a65532483952434a7a6af3ff1fe97ea5
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-18 13:51:22 +0700

    Update getProfile.php

--- getProfile.php
+++ getProfile.php
@@ -43,15 +43,13 @@ curl_close ($ch); */
 
 	$ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL, "https://api.line.me/oauth2/v2.1/revoke");
+	curl_setopt($ch, CURLOPT_URL, "https://api.line.me/v2/profile");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "client_id=1594277893&client_secret=d2a63d13dacf5041464d249127ebf50d
-&access_token=5ZXZrSS8ZkEDGBilHGY2vKyWARc0Yx6O9zOZ/iN24MH39flQZLWt7gvvOY10/LMD3rppnVJBza1RyQIMXJ2vsoPh8i+L2nyIG8y0tlqR/asJiq0gfm1W5wh93re+XESxhwpUoa5q3iZuokzvYqNIcgdB04t89/1O/w1cDnyilFU=
-");
-	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+
 
 	$headers = array();
-	$headers[] = "Content-Type: application/x-www-form-urlencoded";
+	$headers[] = "Authorization: Bearer {5ZXZrSS8ZkEDGBilHGY2vKyWARc0Yx6O9zOZ/iN24MH39flQZLWt7gvvOY10/LMD3rppnVJBza1RyQIMXJ2vsoPh8i+L2nyIG8y0tlqR/asJiq0gfm1W5wh93re+XESxhwpUoa5q3iZuokzvYqNIcgdB04t89/1O/w1cDnyilFU=}";
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 	$result = curl_exec($ch);
