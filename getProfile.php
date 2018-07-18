47209bbd49546fa1a4b031ab2edd3b9137bdbed7
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-18 14:33:52 +0700

    Update getProfile.php

--- getProfile.php
+++ getProfile.php
@@ -3,15 +3,15 @@
 $code = $_GET['code'];
 //$api = "https://api.line.me/oauth2/v2.1/token";
 
-//$authen_login ="grant_type=authorization_code&code=$code;&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1594277893&client_secret=d2a63d13dacf5041464d249127ebf50d";
-$authen_api ="grant_type=authorization_code&code=$code&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1591950917&client_secret=33a71e2a76f6b89b4b31e61f02d93f97";
+$authen_login ="grant_type=authorization_code&code=$code&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1594277893&client_secret=d2a63d13dacf5041464d249127ebf50d";
+//$authen_api ="grant_type=authorization_code&code=$code&redirect_uri=https://majestic-biscayne-65338.herokuapp.com/getProfile.php&client_id=1591950917&client_secret=33a71e2a76f6b89b4b31e61f02d93f97";
 
 
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, "https://api.line.me/oauth2/v2.1/token");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $authen_api);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $authen_login);
 curl_setopt($ch, CURLOPT_POST, 1);
 
 $headers = array();
